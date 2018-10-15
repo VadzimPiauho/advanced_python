55eb0ba9c1e6788991dea942697be6365b0426be
Vadzim Pliauho <Vadzim_Pliauho@epam.com>
2018-10-15 17:08:42 +0300

    fix flake8

--- class.py
+++ class.py
@@ -46,16 +46,15 @@ class Money(object):
         return request["rates"][secondary_exchange_rate]
 
 
-if __name__ == "__main__":
-    x = Money(10, "BYN")
-    y = Money(11)   # define your own default value, e.g. “USD”
-    z = Money(12.34, "EUR")
-    print(z + 3.11*x + y*0.8)   # result in “EUR”
-    # >>543.21 EUR
+x = Money(10, "BYN")
+y = Money(11)   # define your own default value, e.g. “USD”
+z = Money(12.34, "EUR")
+print(z + 3.11*x + y*0.8)   # result in “EUR”
+# >>543.21 EUR
 
-    lst = [Money(10, "BYN"), Money(11), Money(12.01, "JPY")]
+lst = [Money(10, "BYN"), Money(11), Money(12.01, "JPY")]
 
-    s = sum(lst)
+s = sum(lst)
 
-    print(s)    # result in “BYN”
-    # >>123.45 BYN
+print(s)    # result in “BYN”
+# >>123.45 BYN
